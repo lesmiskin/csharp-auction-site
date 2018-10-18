c5486378954b5dca19eddb669f6822b94d4f52f0
Les Miskin <les@lesmiskin.nz>
2018-10-18 21:51:09 +1300

    Better setup script, and stopped VS from starting browser window on "Start without debugging".

--- AuctionSite.Api/Database/create-database-structure.sql
+++ AuctionSite.Api/Database/create-database-structure.sql
@@ -1,11 +1,12 @@
 ﻿-- initial cleanup to permit re-running
 drop table if exists auctions;
 
+-- the table structure proper
 CREATE TABLE auctions (
 	id integer PRIMARY KEY,
 	title text NOT NULL
 );
 
-insert into auctions(id, title) values(1, 'test auction');
-
-select * from auctions;
+-- example data
+insert into auctions(id, title) values(1, 'test auction number 1');
+insert into auctions(id, title) values(2, 'test auction number 2');
